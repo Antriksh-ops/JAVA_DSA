35e14e95efef5316785c7a562a78258b455aac46
Vishu <antriksh058@gmail.com>
2025-09-03 11:18:23 +0530

    Hollow rhombus

---
+++ src/com/company/Prob9.java
@@ -0,0 +1,29 @@
+package com.company;
+// hollow rhombus
+public class Prob9 {
+    public static void main(String[] args) {
+        int n = 5;
+        int m = 5;
+
+        for (int i = 1; i <=n ; i++) {
+
+
+            // spaces
+            for (int j = 1; j <= n - i ; j++) {
+                System.out.print(" ");
+            }
+
+
+            // stars
+            for (int j = 1; j <=m ; j++) {
+                if (i == 1 || j == 1 || i == n || j == m) {
+                    System.out.print("*");
+                }
+                else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
